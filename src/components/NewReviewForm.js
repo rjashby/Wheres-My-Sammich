ee8130991b62bbcf80b1b7598fcd9935f8400cbd
Ryan Ashby <rjapsu160@gmail.com>
2022-05-18 15:34:53 -0700

    update my entire review form to mirror the test form I created - Works with SQL

--- src/components/NewReviewForm.js
+++ src/components/NewReviewForm.js
@@ -1,89 +1,145 @@
 import React from "react"
-import { useFirestore } from 'react-redux-firebase';
+import Axios from 'axios';
+// import { useFirestore } from 'react-redux-firebase';
 
 export default function NewReviewForm() {
 
-  const firestore = useFirestore();
+  // const firestore = useFirestore();
 
-  const [formData, setFormData] = React.useState(
-      {
-        title: "", 
-        sandwich: "", 
-        location: "", 
-        comments: "",
-        stars: "",
-        // user: ,
-        // photo: ,  
-      }
-  ) 
+  // const [formData, setFormData] = React.useState(
+  //     {
+  //       title: "", 
+  //       sandwich: "", 
+  //       location: "", 
+  //       comments: "",
+  //       stars: "",
+  //       // user: ,
+  //       // photo: ,  
+  //     }
+  // ) 
 
-  function handleChange(event) {
-    const {name, value} = event.target
-    setFormData(prevFormData => {
-      return {
-        ...prevFormData,
-        [name]: value
-      }
+  const [title, setTitle] = React.useState("");
+  const [sandwich, setSandwich] = React.useState("");
+  const [location, setLocation] = React.useState("");
+  const [stars, setStars] = React.useState("");
+  const [comments, setComments] = React.useState("");
+
+  const [sandwichList, setSandwichList] = React.useState([]);
+
+  const addSandwich = () => {
+    console.log(title);
+    Axios.post('http://localhost:3001/create', {
+      title: title, 
+      sandwich: sandwich, 
+      location: location, 
+      stars: stars, 
+      comments: comments
+    }).then(() => {
+      setSandwichList([...sandwichList, 
+        {
+        title: title, 
+        sandwich: sandwich, 
+        location: location, 
+        stars: stars, 
+        comments: comments
+        }, 
+      ]);
     })
-  }
+  };
+
+  // const getSandwiches = () => {
+  //   Axios.get('http://localhost:3001/sandwiches').then((response) => {
+  //     setSandwichList(response.data);
+  //   });
+  // }
+
+  // const deleteSandwich = (id) => {
+  //   Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+  //     setSandwichList(
+  //       sandwichList.filter((val) => {
+  //         return val.id !== id;
+  //       })
+  //     );
+  //   });
+  // };
+
+  // function handleChange(event) {
+  //   const {name, value} = event.target
+  //   setFormData(prevFormData => {
+  //     return {
+  //       ...prevFormData,
+  //       [name]: value
+  //     }
+  //   })
+  // }
 
 
-  function addReview(event) {
-    event.preventDefault();
-    return firestore.collection('sandwiches').add(
-      {
-        title: event.target.title.value,
-        sandwich: event.target.sandwich.value,
-        location: event.target.location.value, 
-        stars: event.target.stars.value,
-        comments: event.target.comments.value
-      }
-    );
-  }
+  // function addReview(event) {
+  //   event.preventDefault();
+  //   return firestore.collection('sandwiches').add(
+  //     {
+  //       title: event.target.title.value,
+  //       sandwich: event.target.sandwich.value,
+  //       location: event.target.location.value, 
+  //       stars: event.target.stars.value,
+  //       comments: event.target.comments.value
+  //     }
+  //   );
+  // }
   
-  console.log(formData);
+  // console.log(formData);
   return (
     <React.Fragment>
       <div className="flex justify-center">
         <div className="border-2 border-solid border-black max-w-sm rounded overflow-hidden shadow-lg mb-2 mt-4">
           <h1 className="text-center text-xl">Leave a Review</h1>
-          <form className="bg-white shadow-sm rounded px-8 pt-6 pb-8" onSubmit={addReview}>
+          <form className="bg-white shadow-sm rounded px-8 pt-6 pb-8">
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2" 
             type="text"
             placeholder="Review Title"
-            onChange={handleChange}
-            name="title"
-            value={formData.title} 
+            onChange={(event) => {
+              setTitle(event.target.value)
+            }}
+            // name="title"
+            // value={formData.title} 
             />
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
             type="text"
             placeholder="Sandwich"
-            onChange={handleChange}
-            name="sandwich"
-            value={formData.sandwich}
+            onChange={(event) => {
+              setSandwich(event.target.value)
+            }}
+            // name="sandwich"
+            // value={formData.sandwich}
             />
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
             type="text"
             placeholder="Location"
-            onChange={handleChange}
-            name="location"
-            value={formData.location}
+            onChange={(event) => {
+              setLocation(event.target.value)
+            }}
+            // name="location"
+            // value={formData.location}
             />
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
             type="text"
             placeholder="How Many Stars out of 5?"
-            onChange={handleChange}
-            name="stars"
-            value={formData.stars}
+            onChange={(event) => {
+              setStars(event.target.value)
+            }}
+            // name="stars"
+            // value={formData.stars}
             />
             <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
-                value={formData.comments}
                 placeholder="Comments"
-                onChange={handleChange}
-                name="comments"
+                onChange={(event) => {
+                  setComments(event.target.value)
+                }}
+                // name="comments"
+                // value={formData.comments}
             />
             <div className="flex justify-center">
-              <button className="bg-yellow-500 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full">Submit Review</button>
+              <button className="bg-yellow-500 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full" onClick={addSandwich}>Submit Review</button>
             </div>
           </form>
         </div>
