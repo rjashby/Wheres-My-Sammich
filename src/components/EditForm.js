f2bf018e0df29d714fabb92fd834e1aa44235d78
Ryan Ashby <rjapsu160@gmail.com>
2022-05-19 11:22:43 -0700

    add useNavigate to redirect as needed

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,23 +1,7 @@
 import React from "react"
 import Axios from 'axios';
-import { useNavigate } from "react-router-dom";
-// import { useFirestore } from 'react-redux-firebase';
 
-export default function NewReviewForm() {
-
-  // const firestore = useFirestore();
-
-  // const [formData, setFormData] = React.useState(
-  //     {
-  //       title: "", 
-  //       sandwich: "", 
-  //       location: "", 
-  //       comments: "",
-  //       stars: "",
-  //       // user: ,
-  //       // photo: ,  
-  //     }
-  // ) 
+export default function EditForm() {
 
   const [title, setTitle] = React.useState("");
   const [sandwich, setSandwich] = React.useState("");
@@ -25,86 +9,42 @@ export default function NewReviewForm() {
   const [stars, setStars] = React.useState("");
   const [comments, setComments] = React.useState("");
   const [image, setImage] = React.useState("");
+  const [id, setId] = React.useState(0)
 
   const [sandwichList, setSandwichList] = React.useState([]);
 
-  let navigate = useNavigate();
-
-  const addSandwich = () => {
-    console.log(title);
-    Axios.post('http://localhost:3001/create', {
-      title: title, 
-      sandwich: sandwich, 
-      location: location, 
-      stars: stars, 
-      comments: comments,
-      image: image
-    }).then(() => {
-      setSandwichList([...sandwichList, 
-        {
-        title: title, 
-        sandwich: sandwich, 
-        location: location, 
-        stars: stars, 
-        comments: comments,
-        image: image
-        }, 
-      ]);
-    })
+  const updateReview = (id) => {
+    Axios.put("http://localhost:3001/update", { 
+      title: newTitle,
+      sandwich: newSandwich,
+      location: newLocation,
+      stars: newStars,
+      comments: newComments, id: id }).then(
+      (response) => {
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id === id
+              ? {
+                title: newTitle,
+                sandwich: newSandwich,
+                location: newLocation,
+                stars: newStars,
+                comments: newComments,
+                id: val.id
+                }
+              : val;
+          })
+        );
+      }
+    );
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate('/')
-  };
-
-  // const getSandwiches = () => {
-  //   Axios.get('http://localhost:3001/sandwiches').then((response) => {
-  //     setSandwichList(response.data);
-  //   });
-  // }
-
-  // const deleteSandwich = (id) => {
-  //   Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
-  //     setSandwichList(
-  //       sandwichList.filter((val) => {
-  //         return val.id !== id;
-  //       })
-  //     );
-  //   });
-  // };
-
-  // function handleChange(event) {
-  //   const {name, value} = event.target
-  //   setFormData(prevFormData => {
-  //     return {
-  //       ...prevFormData,
-  //       [name]: value
-  //     }
-  //   })
-  // }
-
-
-  // function addReview(event) {
-  //   event.preventDefault();
-  //   return firestore.collection('sandwiches').add(
-  //     {
-  //       title: event.target.title.value,
-  //       sandwich: event.target.sandwich.value,
-  //       location: event.target.location.value, 
-  //       stars: event.target.stars.value,
-  //       comments: event.target.comments.value
-  //     }
-  //   );
-  // }
-  
-  // console.log(formData);
   return (
     <React.Fragment>
       <div className="flex justify-center">
         <div className="border-2 border-solid border-black max-w-sm rounded overflow-hidden shadow-lg mb-2 mt-4">
-          <h1 className="text-center text-xl">Leave a Review</h1>
-          <form className="bg-white shadow-sm rounded px-8 pt-6 pb-8" onSubmit={handleSubmit}>
+          <h1 className="text-center text-xl">Edit Your Review</h1>
+          <form className="bg-white shadow-sm rounded px-8 pt-6 pb-8">
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2" 
             type="text"
             placeholder="Review Title"
@@ -159,7 +99,7 @@ export default function NewReviewForm() {
             // value={formData.stars}
             required />
             <div className="flex justify-center">
-              <button className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded border-2 border-solid border-black" onClick={addSandwich}>Submit Review</button>
+              <button className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded border-2 border-solid border-black" onClick={updateReview}>Update Review</button>
             </div>
           </form>
         </div>
